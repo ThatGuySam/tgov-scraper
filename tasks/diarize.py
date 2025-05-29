d76de364c7f619fbd46a3a187ffd676db3fdd750
Kendall Werts <kmwerts@gmail.com>
2025-05-28 23:23:38 -0500

    skip no video

--- tasks/diarize.py
+++ tasks/diarize.py
@@ -7,5 +7,8 @@ from src.models.meeting import Meeting
 @task
 def diarize_meeting(meeting: Meeting):
     video_file = download_video(f"{meeting.meeting}_{meeting.date}", str(meeting.video))
-    run_diarization(video_file)
+    if video_file:
+        run_diarization(video_file)
+    else:
+        print("Video file not found")
     # TODO: Update meeting with transcript location
